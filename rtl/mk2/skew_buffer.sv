98509a4a7bfcd916ca73a6e8b7d72fa5957d456c
Danton Klein <dantonmklein@gmail.com>
2026-01-04 02:42:53 -0500

    sometimes things are that deep

--- rtl/mk2/skew_buffer.sv
+++ rtl/mk2/skew_buffer.sv
@@ -64,3 +64,73 @@ always_comb begin
 end
 
 endmodule
+
+
+//i need a way to track the progress of each column being filled, maybe i could use a counter or something
+module reverse_skew_buffer #(
+    parameter ARRAY_SIZE = 8,
+    parameter DATA_WIDTH = 32
+)(
+    input logic clk,
+    input logic rst,
+    input logic enable, //for writing
+    input logic write, //for writing
+    input logic read, //for reading
+
+    //rows are written 
+    input var logic signed [DATA_WIDTH-1:0] data_in [ARRAY_SIZE],
+    output var logic signed [DATA_WIDTH-1:0] data_out [ARRAY_SIZE]
+);
+
+logic[DATA_WIDTH-1:0] shifted_c_buffer[ARRAY_SIZE][ARRAY_SIZE];
+logic[ARRAY_SIZE] write_column;
+logic[$clog2(ARRAY_SIZE)-1:0] read_counter;
+
+logic write_buffer;
+assign write_buffer = enable & write;
+
+always_ff @(posedge clk or posedge rst) begin
+    if(rst) begin
+        for(int i = 0; i < ARRAY_SIZE; i++) begin
+            for(int j = 0; j < ARRAY_SIZE; j++) begin
+                shifted_c_buffer[i][j] <= 0;
+            end
+            if(i == 0) write_column[i] <= 1;
+            else write_column[i] <= 0;
+        end
+        read_counter <= '1;
+    end else begin
+        if(read) begin
+            //upon a read being initiated increment the counter, also reset the write_column buffer
+            read_counter <= read_counter - 1'b1;
+            write_column[0] <= 1;
+            for(int i = 1; i < ARRAY_SIZE; i++) begin
+                write_column[i] <= 0;
+            end
+        end else if(write_buffer) begin
+            for(int i = 0; i < ARRAY_SIZE-1; i++) begin
+                write_column[i+1] <= write_column[i];
+            end
+            //write to a column
+            for(int i = 0; i < ARRAY_SIZE; i++) begin
+                if(write_column[i]) begin
+                    shifted_c_buffer[0][i] <= data_in[i];
+                end
+                for(int j = 1; j < ARRAY_SIZE; j++) begin
+                    shifted_c_buffer[j][i] <= shifted_c_buffer[j-1][i];
+                end
+            end
+        end
+    end
+end
+
+always_comb begin
+    for(int i = 0; i < ARRAY_SIZE; i++) begin
+        if(read) begin //can probably get rid of this if statement
+            data_out[i] = shifted_c_buffer[read_counter][i];
+        end else begin
+            data_out[i] = 0;
+        end
+    end
+end
+endmodule
--- tb/mk2/skew_buffer_tb.sv
+++ tb/mk2/skew_buffer_tb.sv
@@ -51,3 +51,56 @@ module skew_buffer_tb2;
         disable generate_clk;
     end
 endmodule
+
+module reverse_skew_buffer_tb2;
+    parameter int DATA_WIDTH = 32;
+    parameter int ARRAY_SIZE = 4;
+
+    logic clk = 1'b0;
+    logic rst;
+    logic enable;
+    logic write;
+    logic read;
+
+    logic signed [DATA_WIDTH-1:0] data_in [ARRAY_SIZE-1:0];
+    logic signed [DATA_WIDTH-1:0] data_out [ARRAY_SIZE-1:0];
+
+    reverse_skew_buffer #(.DATA_WIDTH(DATA_WIDTH), .ARRAY_SIZE(ARRAY_SIZE)) dut (.*);
+
+    initial begin: generate_clk
+        clk <= 1'b0;
+        forever #5 clk <= ~clk;
+    end
+    
+    initial begin
+        rst <= 1;
+        enable <= 0;
+        write <= 0;
+        read <= 0;
+        @(posedge clk);
+        rst <= 0;
+        @(posedge clk);
+
+        write <= 1;
+        enable <= 1;
+
+        //write to the buffer
+        for(int i = 0; i < ARRAY_SIZE; i++) begin
+            for(int j = 0; j < ARRAY_SIZE; j++) begin
+                data_in[j] <= i + 1;
+            end
+            @(posedge clk);
+        end
+
+        //being reading from the buffer
+        write <= 0;
+        enable <= 0;
+        read <= 1;
+
+        repeat(4) @(posedge clk);
+
+        read <= 0;
+        @(posedge clk);
+        disable generate_clk;
+    end
+endmodule
