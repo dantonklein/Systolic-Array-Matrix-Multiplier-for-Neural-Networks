156cc9cc6929a63c09ce2202436f6ae179662bf4
Danton Klein <dantonmklein@gmail.com>
2026-01-06 03:03:04 -0500

    i hate it when you burp and you throw up in your mouth a bit

--- rtl/mk2/controller.sv
+++ rtl/mk2/controller.sv
@@ -1,5 +1,15 @@
 module controller (
-    
+    input logic clk,
+    input logic rst,
+
+    input logic start,
+    output logic done,
+    output logic ready,
+
+    input logic signed [7:0] a_data[8],
+    input logic signed [7:0] b_data[8],
+
+    output logic signed [31:0] c_data[8]
 );
 
 
